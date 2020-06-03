ec92c14887bc49d326db46f32b2103a31cbba03b
Henrik Stanley Mortensen <hemor@dfds.com>
2020-06-03 15:29:03 +0200

    updated sample code

--- main.go
+++ main.go
@@ -7,6 +7,16 @@ import (
 	"github.com/go-resty/resty/v2"
 )
 
+type agentcloud struct {
+	agentCloudId                  int    `json:"agentCloudId"`
+	internal                      bool   `json:"internal"`
+	name                          string `json:"name"`
+	acquireAgentEndpoint          string `json:"acquireAgentEndpoint"`
+	releaseAgentEndpoint          string `json:"releaseAgentEndpoint"`
+	getAgentDefinitionEndpoint    string `json:"getAgentDefinitionEndpoint"`
+	getAgentRequestStatusEndpoint string `json:"getAgentRequestStatusEndpoint"`
+}
+
 func main() {
 
 	// Get Azure Devops personal access token from environment
@@ -15,15 +25,21 @@ func main() {
 	client := resty.New()
 	// Bearer Auth Token for all request
 	client.SetBasicAuth("", token)
-	resp, err := client.R().Get("https://dev.azure.com/dfds/_apis/distributedtask/agentclouds?api-version=5.1-preview.1")
+	// resp, err := client.R().Get("https://dev.azure.com/dfds/_apis/distributedtask/agentclouds?api-version=6.0-preview.1")
 
-	fmt.Println("Response Info:")
-	fmt.Println("Error      :", err)
-	fmt.Println("Status Code:", resp.StatusCode())
-	fmt.Println("Status     :", resp.Status())
-	fmt.Println("Proto      :", resp.Proto())
-	fmt.Println("Time       :", resp.Time())
-	fmt.Println("Received At:", resp.ReceivedAt())
-	fmt.Println("Body       :\n", resp)
-	fmt.Println()
+	//fmt.Print(resp.Result())
+	client.R().SetResult(agentcloud{})
+	resp, _ := client.R().
+		Get("https://dev.azure.com/dfds/_apis/distributedtask/agentclouds?api-version=6.0-preview.1")
+	fmt.Printf("%#v\n", resp.Result())
+
+	// fmt.Println("Response Info:")
+	// fmt.Println("Error      :", err)
+	// fmt.Println("Status Code:", resp.StatusCode())
+	// fmt.Println("Status     :", resp.Status())
+	// fmt.Println("Proto      :", resp.Proto())
+	// fmt.Println("Time       :", resp.Time())
+	// fmt.Println("Received At:", resp.ReceivedAt())
+	// fmt.Println("Body       :\n", resp)
+	// fmt.Println()
 }
