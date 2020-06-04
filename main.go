50bb25f087f95ca9faa4994ec0160596c076521b
Kim Lindhard <kilin@dfds.com>
2020-06-04 06:31:01 +0200

    use json.Unmarshal to covert http repose to struct

--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/go-resty/resty/v2"
 )
 
-type agentcloud struct {
-	agentCloudId                  int    `json:"agentCloudId"`
-	internal                      bool   `json:"internal"`
-	name                          string `json:"name"`
-	acquireAgentEndpoint          string `json:"acquireAgentEndpoint"`
-	releaseAgentEndpoint          string `json:"releaseAgentEndpoint"`
-	getAgentDefinitionEndpoint    string `json:"getAgentDefinitionEndpoint"`
-	getAgentRequestStatusEndpoint string `json:"getAgentRequestStatusEndpoint"`
+type AgentcloudsResponse struct {
+	Count int `json:"count"`
+	Value []struct {
+		ID                            string `json:"id"`
+		AgentCloudID                  int    `json:"agentCloudId"`
+		Name                          string `json:"name"`
+		Type                          string `json:"type"`
+		Internal                      bool   `json:"internal"`
+		AcquireAgentEndpoint          string `json:"acquireAgentEndpoint"`
+		ReleaseAgentEndpoint          string `json:"releaseAgentEndpoint"`
+		GetAgentDefinitionEndpoint    string `json:"getAgentDefinitionEndpoint"`
+		GetAgentRequestStatusEndpoint string `json:"getAgentRequestStatusEndpoint"`
+	} `json:"value"`
 }
 
 func main() {
@@ -25,21 +31,11 @@ func main() {
 	client := resty.New()
 	// Bearer Auth Token for all request
 	client.SetBasicAuth("", token)
-	// resp, err := client.R().Get("https://dev.azure.com/dfds/_apis/distributedtask/agentclouds?api-version=6.0-preview.1")
 
-	//fmt.Print(resp.Result())
-	client.R().SetResult(agentcloud{})
 	resp, _ := client.R().
 		Get("https://dev.azure.com/dfds/_apis/distributedtask/agentclouds?api-version=6.0-preview.1")
-	fmt.Printf("%#v\n", resp.Result())
 
-	// fmt.Println("Response Info:")
-	// fmt.Println("Error      :", err)
-	// fmt.Println("Status Code:", resp.StatusCode())
-	// fmt.Println("Status     :", resp.Status())
-	// fmt.Println("Proto      :", resp.Proto())
-	// fmt.Println("Time       :", resp.Time())
-	// fmt.Println("Received At:", resp.ReceivedAt())
-	// fmt.Println("Body       :\n", resp)
-	// fmt.Println()
+	agentcloudsResponse := AgentcloudsResponse{}
+	json.Unmarshal(resp.Body(), &agentcloudsResponse)
+	fmt.Printf("%#v\n", agentcloudsResponse)
 }
