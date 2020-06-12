e49502afad425ad12ce76724ee7f15d49c71ab6d
Kim Lindhard <kilin@dfds.com>
2020-06-12 08:22:32 +0200

    awsStorage getLastScrapeStarTime looks in s3

--- aws_storage.go
+++ aws_storage.go
@@ -5,30 +5,61 @@ import (
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"sort"
+	"strings"
+	"time"
 )
 
 type awsStorage struct{}
 
-func (awsStorage) getExistingBuildIDs() []string {
+var datalakeS3Bucket = "dfds-datalake"
+var folderPrefix = "azure-devops/"
 
-	return nil
+func (awsStorage) getLastScrapeStarTime() time.Time {
+
+	awsSession := getAwsSession()
+	svc := s3.New(awsSession)
+
+	//resp, _ := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(datalakeS3Bucket),Prefix: folderPrefix})
+	resp, _ := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(datalakeS3Bucket)})
+
+	var scrapeTimes []string
+	for _, file := range resp.Contents {
+		fmt.Println("Name:         ", *file.Key)
+		scrapeTime := strings.TrimLeft(strings.TrimRight(*file.Key, ".json"), "azure-devops-builds-")
+		scrapeTimes = append(scrapeTimes, scrapeTime)
+	}
+
+	if len(scrapeTimes) == 0 {
+		return time.Date(0, 1, 1, 0, 0, 0, 0, time.UTC)
+	}
+	sort.Strings(scrapeTimes)
+
+	last := scrapeTimes[len(scrapeTimes)-1]
+
+	lastScrapeTime, _ := time.Parse("2006-01-02T15:04:05Z", last)
+
+	return lastScrapeTime
 }
 
-var myBucket = "dfds-datalake"
-
-func (awsStorage) storeBuild(buildID string, fileContent string) {
-
+func getAwsSession() *session.Session {
 	var awsConfig *aws.Config
 	awsConfig = &aws.Config{
 		Region: aws.String("eu-west-1"),
 	}
 
 	// The session the S3 Uploader will use
-	sess := session.Must(session.NewSession(awsConfig))
+	awsSession := session.Must(session.NewSession(awsConfig))
 
-	// Create an uploader with the session and default options
-	uploader := s3manager.NewUploader(sess)
+	return awsSession
+}
+
+func (awsStorage) storeScrapeResult(timeStamp time.Time, fileContent string) {
+
+	awsSession := getAwsSession()
+	uploader := s3manager.NewUploader(awsSession)
 
 	// Create an uploader with the session and custom options
 	// uploader := s3manager.NewUploader(sess, func(u *s3manager.Uploader) {
@@ -36,14 +67,16 @@ func (awsStorage) storeBuild(buildID string, fileContent string) {
 	//     u.Concurrency = 2            // default is 5
 	// })
 
-	//defer f.Close()
 	// Source sample https://paulbradley.org/gos3/
-	buildFileKey := "azure-devops/" + buildID + ".json"
+
+	fileName := "azure-devops-builds-" + timeStamp.Format(time.RFC3339) + ".json"
+
+	buildFileKey := folderPrefix + fileName
 	dataToWrite := []byte(fileContent)
 
 	// Upload the file to S3.
 	result, err := uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String(myBucket),
+		Bucket: aws.String(datalakeS3Bucket),
 		Key:    aws.String(buildFileKey),
 		Body:   bytes.NewReader(dataToWrite),
 	})
