742bfae200c64d53c2ff49083b01d60428ed7994
Kim Lindhard <kilin@dfds.com>
2020-06-14 21:34:20 +0200

    remove old storage functions from disk storage

--- disk_storage.go
+++ disk_storage.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"sort"
@@ -10,34 +9,6 @@ import (
 )
 
 type diskStorage struct{}
-
-func (diskStorage) getExistingBuildIDs() []string {
-	dir, err := os.Getwd()
-	check(err)
-	root := dir + "/existing-builds/"
-
-	fileInfo, err := ioutil.ReadDir(root)
-	check(err)
-
-	var existingBuilds []string
-	for _, file := range fileInfo {
-		existingBuilds = append(existingBuilds, strings.TrimRight(file.Name(), ".json"))
-	}
-
-	return existingBuilds
-}
-func (diskStorage) storeBuild(buildID string, fileContent string) {
-
-	dir, err := os.Getwd()
-	check(err)
-
-	fmt.Print("storing: '" + buildID + "' ")
-
-	fileName := dir + "/existing-builds/" + buildID + ".json"
-	dataToWrite := []byte(fileContent)
-	err = ioutil.WriteFile(fileName, dataToWrite, 0644)
-	check(err)
-}
 
 func (diskStorage) storeScrapeResult(timeStamp time.Time, fileContent string) {
 
