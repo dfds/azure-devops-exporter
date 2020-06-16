fe6c344438c6a84f0495d18faeef0f48e00f4fb6
Henrik Stanley Mortensen <hemor@dfds.com>
2020-06-16 15:03:59 +0200

    fix hardcoded image from kafka-exporter to ado exporter

--- release/kustomize-replace-image-prod.sh
+++ release/kustomize-replace-image-prod.sh
@@ -3,7 +3,7 @@ set -eu -o pipefail
 # Replaces the image in the for-production overlay
 BUILD_ID=$1
 
-(cd ./k8s-orchestration/overlays/for-production; kustomize edit set image azure-devops-exporter/image-placeholder=579478677147.dkr.ecr.eu-central-1.amazonaws.com/ded/kafka-janitor:${BUILD_ID}
+(cd ./k8s-orchestration/overlays/for-production; kustomize edit set image azure-devops-exporter/image-placeholder=579478677147.dkr.ecr.eu-central-1.amazonaws.com/ded/azure-devops-exporter:${BUILD_ID}
 )
 
 sed -i -e 's/resources/bases/g' ./k8s-orchestration/overlays/for-production/kustomization.yaml
