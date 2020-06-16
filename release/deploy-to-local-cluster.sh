424d3d922dde3cb514f304ff467d9ebf3f8e1af5
Henrik Stanley Mortensen <hemor@dfds.com>
2020-06-16 14:00:52 +0200

    move files for pipeline compatability

--- release/deploy-to-local-cluster.sh
+++ release/deploy-to-local-cluster.sh
@@ -2,6 +2,6 @@
 CONTINER_IMAGE=${1}
 
 # kustomize edit set image a=b/c:2
-kustomize build ../k8s-orchestration/overlays/for-development | \
+kustomize build ./k8s-orchestration/overlays/for-development | \
 tee /dev/tty | \
 kubectl apply -f -
--- release/kustomize-replace-image-prod.sh
+++ release/kustomize-replace-image-prod.sh
@@ -3,5 +3,5 @@ set -eu -o pipefail
 # Replaces the image in the for-production overlay
 BUILD_ID=$1
 
-(cd ../k8s-orchestration/overlays/for-production; kustomize edit set image azure-devops-exporter/image-placeholder=579478677147.dkr.ecr.eu-central-1.amazonaws.com/ded/kafka-janitor:${BUILD_ID}
+(cd ./k8s-orchestration/overlays/for-production; kustomize edit set image azure-devops-exporter/image-placeholder=579478677147.dkr.ecr.eu-central-1.amazonaws.com/ded/kafka-janitor:${BUILD_ID}
 )
