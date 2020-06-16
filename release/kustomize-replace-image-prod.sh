50df2b83c531bcd501b9afef03ff2f6f6b284324
Henrik Stanley Mortensen <hemor@dfds.com>
2020-06-16 14:58:07 +0200

    Fix kustomize set changes bases to resources

--- release/kustomize-replace-image-prod.sh
+++ release/kustomize-replace-image-prod.sh
@@ -5,3 +5,5 @@ BUILD_ID=$1
 
 (cd ./k8s-orchestration/overlays/for-production; kustomize edit set image azure-devops-exporter/image-placeholder=579478677147.dkr.ecr.eu-central-1.amazonaws.com/ded/kafka-janitor:${BUILD_ID}
 )
+
+sed -i -e 's/resources/bases/g' ./k8s-orchestration/overlays/for-production/kustomization.yaml
