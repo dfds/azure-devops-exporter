1d7cbf1ccfac93a64f4a2a26a709310726ce12f8
Henrik Stanley Mortensen <hemor@dfds.com>
2020-06-18 11:00:10 +0200

    Add error hanlding of empty build string.

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/go-resty/resty/v2"
 	"os"
 	"strings"
 	"time"
+
+	"github.com/go-resty/resty/v2"
 )
 
 func main() {
@@ -17,12 +18,15 @@ func main() {
 	//projectIDs := []string{"136d92f4-a14a-422c-9f0e-230f6dbd90b1","785336a7-e841-46ba-b632-5092b88c7907"}
 
 	storage := awsStorage{}
+	//storage := diskStorage{}
 
 	buildStringsChannel := make(chan string)
 
 	lastScrapeStartTime := storage.getLastScrapeStartTime()
+	//lastScrapeStartTime, _ := time.Parse("2006-01-02T15:04:05Z", "2020-06-18T06:45:19Z")
 
 	scrapeStartTime := time.Now().UTC()
+	//scrapeStartTime, _ := time.Parse("2006-01-02T15:04:05Z", " 2020-06-18T06:50:30Z")
 
 	timeDifference := scrapeStartTime.Sub(lastScrapeStartTime)
 	fmt.Println("Scraping finished builds between: '" + lastScrapeStartTime.Format(time.RFC3339) + "' and '" + scrapeStartTime.Format(time.RFC3339) + "' a HH:MM:SS " + time.Time{}.Add(timeDifference).Format("15:04:05") + " difference")
@@ -45,6 +49,9 @@ func main() {
 	projectsBuildStrings := make([]string, 0)
 	for i := 1; i <= len(projectIDs); i++ {
 		projectBuildsString := <-buildStringsChannel
+		if len(projectBuildsString) == 0 {
+			panic("No build string should be empty (0)")
+		}
 		if projectBuildsString != "{\"count\":0,\"value\":[]}" {
 			projectBuildsString := removeWrapperObject(projectBuildsString)
 			projectsBuildStrings = append(projectsBuildStrings, projectBuildsString)
